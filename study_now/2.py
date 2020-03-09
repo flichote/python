a881d2ddb8af990fb95961006450491ecc5a4f5c
flichote <83833879@qq.com>
2020-03-09 20:33:57 +0800

    leetcode em

---
+++ study_now/2.py
@@ -0,0 +1,19 @@
+"""
+实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
+"""
+class Solution(object):
+    def isUnique(self,astr):
+        """
+        :type astr:str
+        :rtype: bool
+        :param astr:
+        :return:
+        """
+        if len(astr) == len(set(astr)):
+            print("true")
+        else:
+            print("false")
+
+astr = 'aasdf'
+s = Solution()
+s.isUnique(astr)
