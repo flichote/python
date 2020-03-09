97678915faebc8d742ea9708d87dd33637ec98b5
flichote <83833879@qq.com>
2020-03-09 21:30:28 +0800

    实现一个算法，确定一个字符串 s 的所有字符是否全都不同。

--- study_now/2_1.py
+++ study_now/2_1.py
@@ -1,6 +1,7 @@
 """
 实现一个算法，确定一个字符串 s 的所有字符是否全都不同。
 """
+
 class Solution(object):
     def isUnique(self,astr):
         """
@@ -9,11 +10,14 @@ class Solution(object):
         :param astr:
         :return:
         """
-        if len(astr) == len(set(astr)):
-            print("true")
-        else:
-            print("false")
+        for i in range(0,(len(astr)-1)):
+            if astr[i] in astr[i+1]:
+                # break
+                print("false")
+            print('true')
+            break
 
-astr = 'assdf'
+astr = 'aabv'
 s = Solution()
 s.isUnique(astr)
+
