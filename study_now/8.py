af0ca6c6b1228b6df5d40dd6ae3eb6a6c5a5f957
flichote <83833879@qq.com>
2020-03-13 22:34:02 +0800

    给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。

---
+++ study_now/8.py
@@ -0,0 +1,22 @@
+"""
+给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+"""
+
+x = -123
+a = []
+y = abs(x)
+number1 = 0
+str1 = str(y)
+for i in str(y):
+
+    a.insert(0, int(i))
+    if a[-1] == 0:
+        a.pop()
+
+for i in range(len(a)):
+    number1 += a[i]*10**(len(a)-i-1)
+if x >=2147483647 or x <=-2147483648 or number1>=2147483647 or number1<=-2147483648:
+    number1 = 0
+if x < 0:
+    number1 = number1 * (-1)
+print(number1)
